55b762e5d4bafed87c038033b382bbe550eb80d7
kevincornish <kevinz_c@live.com>
2020-06-13 17:13:07 +0100

    update

---
+++ README.md
@@ -0,0 +1,12 @@
+# optrelparser
+2 Inspection Machines
+- 1 Ampoule
+- 1 Vial
+Generate pdf reports
+
+#Todo
+Export PDF strings to database
+Pick report to search ampoule/vial
+Search PDF's via batch number
+Search common reject %'s
+look at trends
--- pdf2py.py
+++ pdf2py.py
@@ -1,23 +1,23 @@
 import pdftotext
 
-# Load PDF
+# open PDF
 with open("190304A.pdf", "rb") as f:
     pdf = pdftotext.PDF(f)
 
-# How many pages?
+# print pages
 print(len(pdf))
 
-# Iterate over all the pages
+# split each page in the pdf
 for page in pdf:
     print(page)
 
-# Read some individual pages
+# read individual pages
 print(pdf[0])
 print(pdf[1])
 
-# Read all the text into one string
+# join all text into one string
 print("\n\n".join(pdf))
 
-# Save all text to a txt file.
+# save joined txt to a output file
 with open('output.txt', 'w') as f:
     f.write("\n\n".join(pdf))

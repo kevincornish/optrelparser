75b71db737c5ab85d7db1acd4cdc680bb7a623f8
kevincornish <kevinz_c@live.com>
2020-06-13 21:49:15 +0100

    search

---
+++ search.py
@@ -0,0 +1,15 @@
+import sqlalchemy as db
+#connect to db
+engine = db.create_engine('sqlite:///reports.sqlite')
+connection = engine.connect()
+metadata = db.MetaData()
+#load the tables
+vials = db.Table('vials', metadata, autoload=True, autoload_with=engine)
+ampoules = db.Table('ampoules', metadata, autoload=True, autoload_with=engine)
+
+search = input("Product search:")
+v = vials.select().where(vials.c.product == search)
+result = connection.execute(v)
+
+for row in result:
+   print ("id:", row[vials.c.id], "; product:", row[vials.c.product])

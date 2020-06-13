91c65f24b7d25cec9c27b229fc7b664911850901
kevincornish <kevinz_c@live.com>
2020-06-13 17:51:57 +0100

    test

--- README.md
+++ README.md
@@ -4,9 +4,10 @@
 - 1 Vial
 Generate pdf reports
 
-#Todo
-Export PDF strings to database
-Pick report to search ampoule/vial
-Search PDF's via batch number
-Search common reject %'s
-look at trends
+Todo
+------
+1. Export PDF strings to database
+2. Pick report to search ampoule/vial
+3. Search PDF's via batch number or name eg 'Magnesium Sulfate', will then pull up all Magnesium Sulfate batches and be able to filter through them.
+4. Search common reject %'s
+5. look at trends
--- pdf2txt.py
+++ pdf2txt.py
@@ -1,20 +1,28 @@
 from lxml import html
 import codecs
 import os
+import sqlalchemy as db
+#connect to db
+engine = db.create_engine('sqlite:///reports.sqlite')
+connection = engine.connect()
+metadata = db.MetaData()
+#load the tables
+vials = db.Table('vials', metadata, autoload=True, autoload_with=engine)
+amps = db.Table('ampoules', metadata, autoload=True, autoload_with=engine)
 
-#todo
+
 # ask if user wants to convert pdfs
 # if yes convert the pdf to text/html
 #os.system("pdf2txt -o output.html *.pdf")
 
 # open file
-file = codecs.open("output.html", "r", "utf-8")
-data = file.read()
-html_file = html.fromstring(data)
+#file = codecs.open("output.html", "r", "utf-8")
+#data = file.read()
+#html_file = html.fromstring(data)
 # get the text from <div><span>text</span><div>
-extracted_data = html_file.xpath('//div//span/text()')
+#extracted_data = html_file.xpath('//div//span/text()')
 # return array of elements
-for elm in extracted_data:
-    line_elements = elm.split()
-    print(line_elements)
-file.close();
+#for elm in extracted_data:
+#    line_elements = elm.split()
+#    print(line_elements)
+#file.close();

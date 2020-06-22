6a01a2a18432cded4f116ada8cea7e94aa1f55cb
Billy Reynolds <breynolds@amplify-nation.com>
2020-06-22 09:05:56 -0400

    Add comments

--- reports/views.py
+++ reports/views.py
@@ -3,6 +3,10 @@ from .models import Vial
 
 
 class VialListView(ListView):
+    # You can customize things in this class like pagination and ordering and such very easily.
+    # Check the link, this resource is key for django.
+    # http://ccbv.co.uk/projects/Django/3.0/django.views.generic.list/ListView/
+    # Don't forget to register your view in optrel/urls.py
     model = Vial
     queryset = Vial.objects.all()
     context_object_name = 'vials'

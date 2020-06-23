e9b8aedb87f66976c44899de746f88dcf7a5fe82
kevincornish <kevinz_c@live.com>
2020-06-23 06:07:35 +0100

    added django filter

--- reports/tables.py
+++ reports/tables.py
@@ -1,5 +1,6 @@
 import django_tables2 as tables
 from .models import Vial, Ampoule
+import django_filters
 
 class VialTable(tables.Table):
     class Meta:
@@ -12,3 +13,14 @@ class AmpouleTable(tables.Table):
         model = Ampoule
         template_name = "django_tables2/bootstrap.html"
         fields = ("username", "product_id", "recipe", "batch_number", "batch_name", "start_date", "end_date", "inspected", "accepted", "rejected", "technical_rejects")
+
+
+class VialFilter(django_filters.FilterSet):
+    class Meta:
+        model = Vial
+        fields = ["username", "product_id", "recipe", "batch_number", "batch_name"]
+
+class AmpouleFilter(django_filters.FilterSet):
+    class Meta:
+        model = Ampoule
+        fields = ["username", "product_id", "recipe", "batch_number", "batch_name"]
--- reports/views.py
+++ reports/views.py
@@ -1,20 +1,23 @@
-from django_tables2 import SingleTableView
+from django_filters.views import FilterView
+from django_tables2 import SingleTableMixin
 from django.views.generic import ListView
 from .models import Vial, Ampoule
-from .tables import VialTable, AmpouleTable
+from .tables import VialTable, AmpouleTable, VialFilter, AmpouleFilter
 
 
-class VialListView(SingleTableView):
+class VialListView(SingleTableMixin, FilterView):
     model = Vial
     queryset = Vial.objects.all()
     context_object_name = 'vials'
     table_class = VialTable
     template_name = 'reports/vials_list.html'
+    filterset_class = VialFilter
 
 
-class AmpouleListView(SingleTableView):
+class AmpouleListView(SingleTableMixin, FilterView):
     model = Ampoule
     queryset = Ampoule.objects.all()
     context_object_name = 'ampoules'
     table_class = AmpouleTable
     template_name = 'reports/ampoules_list.html'
+    filterset_class = AmpouleFilter

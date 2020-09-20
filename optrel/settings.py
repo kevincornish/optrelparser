0ef926fd50dddaa7f92139acb7a9cf09ddff756d
Kev <kevinz_c@live.com>
2020-09-20 14:51:07 +0100

    set L10N & I18N ti false, TZ = UTC correctly

--- optrel/settings.py
+++ optrel/settings.py
@@ -124,11 +124,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-uk'
 
-TIME_ZONE = 'Europe/London'
+TIME_ZONE = 'UTC'
 
-USE_I18N = True
+USE_I18N = False
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
 
